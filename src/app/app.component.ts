dc91fc437654b753539c6a5de0bd41ee10497cfc
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2023-11-06 15:46:25 +0100

    implementation add quotation input, add show all quotes, implementation add votes, implementation rankings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import {Quotation} from './models/quotation';
+import {QUOTES} from './models/data-base';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  showForm = false;
+  quotes: Quotation[] = QUOTES;
+  quotation: Quotation = {author: '', sentence: '', votes: 0};
+
+  onSwitchForm(): void {
+    this.showForm = !this.showForm;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {author: '', sentence: '', votes: 0};
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
+}
